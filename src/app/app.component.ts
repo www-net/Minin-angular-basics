60b06eb88bcef315dba4b4e02c6b7af57f225bf3
www-net <78052177+www-net@users.noreply.github.com>
2023-08-30 15:20:29 +0300

    native pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  isVisible = true
+  e: number = Math.E
+  str = 'hello world'
+  slice = '0123456789'
+  date: Date = new Date();
+  float = 0.42
+
+  obj = {
+    a: 1,
+    b: {
+      c: 2,
+      d: {
+        e: 3,
+        f: 4
+      }
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { StyleDirective } from './directives/style.directive';
-import { IfnotDirective } from './directives/ifnot.directive';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    StyleDirective,
-    IfnotDirective,
   ],
   imports: [
     BrowserModule,
