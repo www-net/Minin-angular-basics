c85ad87f9a1338f8fb4fe0474ed531ce225c62af
www-net <78052177+www-net@users.noreply.github.com>
2023-08-31 11:51:45 +0300

    custom pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Post {
+  title: string
+  text: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -22,4 +27,21 @@ export class AppComponent {
       }
     }
   }
+
+  search = ''
+  searchField = 'title'
+
+  posts: Post[] = [
+    {title: 'Beer', text: 'Самое лучшее пиво в мире'},
+    {title: 'Bread', text: 'The best bread'},
+    {title: 'Javascript', text: 'The best language'}
+  ]
+
+  addPost() {
+    this.posts.unshift({
+       title: 'Angular',
+       text: 'Vladilen Angular course'
+    })
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { MultByPipe } from './pipes/mult-by.pipe';
+import { ExMarksPipe } from './pipes/ex-marks.pipe';
+import { FilterPipe } from './pipes/filter.pipe';
 
 
 
@@ -10,6 +13,9 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
+    MultByPipe,
+    ExMarksPipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
